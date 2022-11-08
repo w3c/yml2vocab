425692eefe9cdedf1b5cd0003ccce86dfb14a867
Ivan Herman <ivan@ivan-herman.net>
2022-11-08 11:38:53 +0100

    Added a proper command line parse

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as yml2vocab from './index';
+import { Command }    from 'commander';
 
 /**
  * Entry point for running the script on the command line: 
@@ -12,19 +13,24 @@ import * as yml2vocab from './index';
  * The default is `vocabulary`
  * 
  */
-import { promises as fs } from 'fs';
-
 async function main() {
-    const get_fname = () : string => {
-        if (process.argv.length > 2) {
-            const vocab_name = process.argv[2];
-            return vocab_name.endsWith('.yml') ? vocab_name.slice(0,-4) : vocab_name;
-        } else {
-            return 'vocabulary'
-        }  
-    }
-    const fname = get_fname();
-    await yml2vocab.generate_vocabulary_files(fname, "template.html");
+    const program = new Command();
+    program
+        .name('yml2vocab')
+        .description('Convert a simple vocabulary, defined in YAML, to formal Turtle, JSON-LD, and HTML/RDFa files.')
+        .usage('[options]')
+        .option('-v --vocab [vocab]')
+        .option('-t --template [template]')
+        .on('--help', () => {
+            console.log('\nvocab:     vocabulary file name, defaults to "vocabulary.yml"\ntemplate:  template file name, defaults to "template.html"')
+        })
+        .parse(process.argv);
+    
+    const options = program.opts();
+    
+    const vocabulary = options.vocab ? options.vocab : 'vocabulary.yml';
+    const template = options.template ? options.template : 'template.html'; 
+    await yml2vocab.generate_vocabulary_files(vocabulary,template);
 }
 
 // At some point, node.js will allow to have async calls at the top level, and this extra function will
